711e5f50f2bea204db361f499b2636390b2b6388
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-03-26 12:01:21 -0400

    added a test case

--- tail.js
+++ tail.js
@@ -5,14 +5,14 @@ const assertEqual = function (actual, expected) {
       console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
   }
 };
-let tailArr = [];
+let tailArr = []
 const tail = function(arr){
   for (let i = 1; i < arr.length; i++){
     tailArr.push(arr[i]);
   }
   return tailArr;
 }
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(result);
-assertEqual(result, ['Lighthouse', 'Labs']); // => will always fail!
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
