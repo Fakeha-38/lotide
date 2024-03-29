bb0467b64daf669e7154350a7ddb4b6943449cc4
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-03-29 08:24:11 -0400

    removed all indentation errors

--- tail.js
+++ tail.js
@@ -1,17 +1,17 @@
 const assertEqual = function (actual, expected) {
-  if(actual === expected){
-      console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
   } else {
-      console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
+    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
   }
 };
-let tailArr = []
-const tail = function(arr){
-  for (let i = 1; i < arr.length; i++){
+let tailArr = [];
+const tail = function(arr) {
+  for (let i = 1; i < arr.length; i++) {
     tailArr.push(arr[i]);
   }
   return tailArr;
-}
+};
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
