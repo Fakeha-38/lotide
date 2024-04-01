37c0ad7e5cbe0f88c57cc3263fae46931ff8936a
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-04-01 00:39:59 -0400

    AI suggestions added

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,7 +1,7 @@
 const eqArrays = function(arr1, arr2) {
   let setResult = true;
   if (arr1.length === arr2.length) {
-    for (let el = 0; el <= arr1.length; el++) {
+    for (let el = 0; el < arr1.length; el++) {
       if (arr1[el] !== arr2[el]) {
         setResult = false;
         return setResult;
@@ -13,31 +13,25 @@ const eqArrays = function(arr1, arr2) {
   setResult = false;
   return setResult;
 };
-// console.log(eqArrays(["red", "green"], ["red", "glue"]));
 const eqObjects = function(obj1, obj2) {
   let setResult = true;
-  for (let key in obj1) {
-    if (typeof obj1[key] === "object") {
-      setResult = eqArrays(obj1[key], obj2[key]);
-    } else if (obj1[key] !== obj2[key]) {
-      setResult = false;
-      return setResult;
+  if (Object.keys(obj1).length === Object.keys(obj2).length) {
+    for (let key in obj1) {
+      if (Array.isArray(obj1[key])) {
+        setResult = eqArrays(obj1[key], obj2[key]);
+      } else if (obj1[key] !== obj2[key]) {
+        setResult = false;
+        return setResult;
+      }
     }
-  }
-  for (let key in obj2) {
-    if (typeof obj2[key] === "object") {
-      setResult = eqArrays(obj1[key], obj2[key]);
-    } else if (obj1[key] !== obj2[key]) {
-      setResult = false;
-      return setResult;
-    }
+  } else {
+    setResult = false;
+    return setResult;
   }
   return setResult;
 };
 const assertObjectsEqual = function(obj1, obj2) {
   const inspect = require("util").inspect;
-  // console.log(`Example label: ${inspect(obj1)}`);
-
   if (eqObjects(obj1, obj2)) {
     console.log(
       `✅✅✅ Assertion passed: ${inspect(obj1)} === ${inspect(obj2)}`,
@@ -47,7 +41,6 @@ const assertObjectsEqual = function(obj1, obj2) {
       `❌❌❌ Assertion failed: ${inspect(obj1)} !== ${inspect(obj2)}`,
     );
   }
-  // Implement me!
 };
 const shirtObject = { color: ["red", "green"], size: "medium" };
 const anotherShirtObject = { size: "medium", color: ["red", "green"] };
