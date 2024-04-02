0d026c16bbdb62f50bb46ae3de8eb4deb4ddb4b6
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-04-02 13:33:13 -0400

    map for maping over array is added

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+
 const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
     for (let el = 0; el <= arr1.length; el++) {
@@ -16,4 +17,17 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log(`❌❌❌ Assertion failed: ${arr1} !== ${arr2}`);
   }
 };
-assertArraysEqual([34, 56], [34, 56]);
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, (word) => word[0]);
+console.log(results1);
+const expected1 = ['g', 'c', 't', 'm', 't'];
+assertArraysEqual(results1, expected1);
+assertArraysEqual(results1, ['g', 'p', 't', 'm', 't']);
