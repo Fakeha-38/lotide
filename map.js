e3c56ca472551e456e9079c980c26be8d5860d30
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-04-02 13:36:31 -0400

    loop conditional issue removed from the eqArrays function

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
-    for (let el = 0; el <= arr1.length; el++) {
+    for (let el = 0; el < arr1.length; el++) {
       if (arr1[el] !== arr2[el]) {
         return false;
       }
